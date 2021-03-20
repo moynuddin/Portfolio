3ed5c2e1a79e540abae2dcda9094397e7c1ea37f
moinuddin <moin.uddin314@gmail.com>
2021-03-21 04:21:01 +0530

    animation hamburger

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('burger') burger: ElementRef;
   isMenuCollapsed: Boolean = true;
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggel() {
+    const open = this.burger.nativeElement;
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+    if (this.isMenuCollapsed) {
+      open.classList.remove('open');
+    } else {
+      open.classList.add('open');
+    }
+  }
 }
